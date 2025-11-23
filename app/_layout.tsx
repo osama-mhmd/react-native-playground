d251dee39bf1306fb772d7139a8bd8cdb375c83a
Osama Mohammed <docosmo010@gmail.com>
2025-11-23 11:14:39 +0200

    feat: app name + theming + disabling the header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,17 @@
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
 import { Stack } from "expo-router";
+import { useColorScheme } from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  const colorScheme = useColorScheme();
+
+  return (
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <Stack screenOptions={{ headerShown: false }} />
+    </ThemeProvider>
+  );
 }
